ad972f312734afc49c685cb0b7367259e6651c37
U-DANI\misan <misankovich@gmail.com>
2024-12-28 18:03:33 -0500

    return clear object

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ load_dotenv()
 
 api_key = os.getenv("api_key")
 
-def get_posts(symbol):
+def get_quote(symbol):
     url = f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={api_key}'
 
     try:
         response = requests.get(url)
 
         if response.status_code == 200:
-            posts = response.json()
-            return posts
+            quote = response.json()
+            data = {
+                "previous_close": quote["pc"],
+                "current_price": quote["c"],
+                "high": quote["h"],
+                "low": quote["l"],
+                "open": quote["o"],
+                "timestamp": quote["t"],
+                "percent_change": quote["dp"],
+                "change": quote["d"],
+            }
+            return data
         else:
             print('Error: ', response.status_code)
             return None
@@ -22,5 +32,5 @@ def get_posts(symbol):
         print('Error: ', e)
         return None
 
-result = get_posts('ALLY')
+result = get_quote('ALLY')
 print(result)
