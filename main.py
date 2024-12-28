f9125ec425697035bcb22989c64b665197c42096
U-DANI\misan <misankovich@gmail.com>
2024-12-28 17:55:09 -0500

    initial api call to get stock price

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-def get_posts():
-    
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+api_key = os.getenv("api_key")
+
+def get_posts(symbol):
+    url = f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={api_key}'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+        else:
+            print('Error: ', response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print('Error: ', e)
+        return None
+
+result = get_posts('ALLY')
+print(result)
