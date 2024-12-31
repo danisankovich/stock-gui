7588d1fb4a3cfdef3eb906952840338fc682be8e
danisankovich <misankovich@gmail.com>
2024-12-30 23:03:03 -0500

    ask about company prospects

--- aiRequest.py
+++ aiRequest.py
@@ -9,8 +9,8 @@ ai_key = os.getenv("ai_key")
 api = OpenAI(api_key=ai_key, base_url='https://api.aimlapi.com')
 system_prompt = "You are a Stock Advisor. Please be descriptive and helpful."
 
-def main():
-    user_prompt = input('What would you like to ask? ')
+def ask(symbol):
+    user_prompt = f"As a stock advisor giving market advice to a client, what can you tell me about the short-term and long-term prospects of the company with the symbol {symbol}?"
     completion = api.chat.completions.create(
         model="mistralai/Mistral-7B-Instruct-v0.2",
         messages=[
@@ -23,8 +23,5 @@ def main():
 
     response = completion.choices[0].message.content
 
-    print("User:", user_prompt)
-    print("AI:", response)
-
-if __name__ == "__main__":
-    main()
+    # print("User:", user_prompt)
+    return response
--- gui.py
+++ gui.py
@@ -1,8 +1,11 @@
 import PySimpleGUI as psg
 from main import get_quote
+from aiRequest import ask
 
 layout = [
     [psg.Text(text='Symbol: '), psg.Input(key='-SYMBOL-')],
+    [psg.Button('Search')],
+    [psg.Text(text='Tell Me About: '), psg.Input(key='-COMPANY-')],
     [psg.Button('Search')]
 ]
 
@@ -12,8 +15,14 @@ while True:
     event, values = window.read()
     if (values and '-SYMBOL-' in values and isinstance(values['-SYMBOL-'], str)):
         symbol = values['-SYMBOL-']
-        quote_data = get_quote(symbol.upper())
-        print(quote_data)
+        if symbol:
+            quote_data = get_quote(symbol.upper())
+            print(quote_data)
+    if (values and '-COMPANY-' in values and isinstance(values['-COMPANY-'], str)):
+        symbol = values['-COMPANY-']
+        if symbol:
+            ai_response = ask(symbol.upper())
+            print(ai_response)
     if event in (None, 'Exit'):
         break
 
